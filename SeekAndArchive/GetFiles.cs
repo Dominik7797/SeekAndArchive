7680f164a9ab2cc1e0235a86018cb02c22beb1f8
Nyerges Dominik <dommi7797@gmail.com>
2020-11-16 15:35:54 +0100

    Add project files.

---
+++ SeekAndArchive/GetFiles.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeekAndArchive
+{
+    class GetFiles
+    {
+        List<string> searchResult = new List<string>();
+
+        public void GetFilesBySearch()
+        {
+            Console.WriteLine("Give a filename eg:Text.rtf");
+            var filename = Console.ReadLine();
+            Console.WriteLine(@"Give a directory to look in. eg: C:\Testing\");
+            var directory = Console.ReadLine();
+            var counter = 0;
+
+            try
+            {
+                 var allFiles = Directory.GetFiles(directory, filename, SearchOption.AllDirectories);
+                foreach (var file in allFiles)
+                {
+                    counter++;
+                    Console.WriteLine(file);
+                    searchResult.Add(file);
+                }
+
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Files found:" + counter);
+            }
+        }
+
+        public void SerializeObjectToTxt(String path)
+        {
+
+            FileStream file = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            try
+            {
+                formatter.Serialize(file, searchResult);
+                Console.WriteLine("Saved.");
+            }
+            catch (SerializationException e)
+            {
+                Console.WriteLine("Failed to serialize. Reason: " + e.Message);
+                throw;
+            }
+            finally
+            {
+                file.Close();
+            }
+        }
+
+        public List<string> DeserializeFromTxtToObject(String path)
+        {
+
+            // Open the file containing the data that you want to deserialize.
+            FileStream fs = new FileStream(path, FileMode.Open);
+            try
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                // Deserialize the hashtable from the file and
+                // assign the reference to the local variable.
+                var prevSearch = (List<string>)formatter.Deserialize(fs);
+            }
+            catch (SerializationException e)
+            {
+                Console.WriteLine("Failed to deserialize. Reason: " + e.Message);
+                throw;
+            }
+            finally
+            {
+                fs.Close();
+            }
+
+            return searchResult;
+
+        }
+    }
+}
---
+++ SeekAndArchive/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace SeekAndArchive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetFiles getFiles = new GetFiles();
+            getFiles.GetFilesBySearch();
+
+            Console.WriteLine("For new search press n, to save press s, to reload prev save press r, to exit press e");
+            var option = Console.ReadLine();
+
+            while (option != "e")
+            {
+                if (option == "n")
+                {
+                    getFiles.GetFilesBySearch();
+                }
+
+                if (option == "s")
+                {
+                    //todo get path without full path.
+                    getFiles.SerializeObjectToTxt(@"C:\Users\dommi\source\repos\SeekAndArchive\SeekAndArchive\data.txt");
+                }
+
+                if (option == "r")
+                {
+                     var prevSearch = getFiles.DeserializeFromTxtToObject(@"C:\Users\dommi\source\repos\SeekAndArchive\SeekAndArchive\data.txt");
+                    foreach(var prevFile in prevSearch)
+                    {
+                        Console.WriteLine(prevFile);
+                    }
+                }
+
+                Console.WriteLine("For new search press n, to save press s, to reload prev save press r, to exit press e");
+                option = Console.ReadLine();
+            }
+        }
+    }
+}
